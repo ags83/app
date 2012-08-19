e0e282c4058edc557aac8305199e2abf471da442
Aaron Shaw <a.shaw83@hotmail.com>
2012-08-19 17:51:35 +0700

    next

--- venv/Scripts/pilprint.py
+++ venv/Scripts/pilprint.py
@@ -1,5 +1,5 @@
 #!C:\Users\Aaron\Desktop\webwork\rentalsite\venv\Scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'PIL==1.1.7',u'pilprint.py'
-__requires__ = 'PIL==1.1.7'
+# EASY-INSTALL-SCRIPT: 'pil==1.1.7','pilprint.py'
+__requires__ = 'pil==1.1.7'
 import pkg_resources
-pkg_resources.run_script('PIL==1.1.7', u'pilprint.py')
+pkg_resources.run_script('pil==1.1.7', 'pilprint.py')
