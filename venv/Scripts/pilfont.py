c252e365c77f15d6a34c9871a18bb2373b28fd3a
Aaron Shaw <a.shaw83@hotmail.com>
2012-08-19 17:44:11 +0700

    first go

---
+++ venv/Scripts/pilfont.py
@@ -0,0 +1,5 @@
+#!C:\Users\Aaron\Desktop\webwork\rentalsite\venv\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'PIL==1.1.7',u'pilfont.py'
+__requires__ = 'PIL==1.1.7'
+import pkg_resources
+pkg_resources.run_script('PIL==1.1.7', u'pilfont.py')
